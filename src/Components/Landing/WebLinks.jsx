b27bd85cf8d35dc1d65abe9b9d97d7ae331d4eea
Michael Fernandes <fernb0t@Kaia-Lynnes-MBP.localdomain>
2021-02-20 18:59:27 -0800

    Neato animation

--- src/Components/Landing/WebLinks.jsx
+++ src/Components/Landing/WebLinks.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 function WebLinks(props) {
   return (
-    <div>
-      <span className="web-link"><a href="https://www.github.com/michael-fernandes">Github</a></span>
-      <span className="web-link"><a href="https://www.linkedin.com/in/mike-fernandes/">LinkedIn</a></span>
+    <div className="web-links-container">
+      <span className="web-link"> 
+        <a href="https://www.github.com/michael-fernandes">Github</a>
+      </span>
+      <span className="web-link">
+        <a href="https://www.linkedin.com/in/mike-fernandes/">LinkedIn</a>
+      </span>
+      <span className="web-link web-link-email">
+        <a href="https://www.linkedin.com/in/mike-fernandes/">m.fern93@gmail.com</a></span>
     </div>
   );
 }
