c7cfa8cd7e16cfbb8b4f619dee0173184ef26c57
Michael Fernandes <fernb0t@Kaia-Lynnes-MBP.localdomain>
2021-04-03 17:39:06 -0700

    Backgrounds for evveryone

---
+++ src/static/backgrounds/index.js
@@ -0,0 +1,6 @@
+import background1 from './background-01.svg';
+import background2 from './background-02.svg';
+import background3 from './background-03.svg';
+import background4 from './background-04.svg';
+
+export { background1, background2, background3, background4 };
