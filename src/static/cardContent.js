e50e2c819816b7de43d0aec35576b2efc164d88f
Michael Fernandes <fernb0t@Kaia-Lynnes-MBP.localdomain>
2021-04-03 17:39:20 -0700

    Add static content

---
+++ src/static/cardContent.js
@@ -0,0 +1,48 @@
+import * as img from '../static/backgrounds';
+
+export default [
+  { 
+    style: {
+      color: '#FFA600', 
+      background: img.background1,
+    },
+    title: 'PhysioMap', 
+    description: 'A collaboration done with Physical Therapy Researchers to create a patient-centered app For communicating fall-risk to geriatric patients.',
+    links: [
+      {url: "https://github.com/michael-fernandes", text: "Code"}
+    ]
+  },
+  { 
+    style: {
+      color: '#00E08E', 
+      background: img.background3,
+    },
+    title: 'PhysioMap', 
+    description: 'A collaboration done with Physical Therapy Researchers to create a patient-centered app For communicating fall-risk to geriatric patients.',
+    links: [
+      {url: "https://github.com/michael-fernandes", text: "Code"}
+    ]
+  }, 
+  { 
+    style: {
+      color: '#FF007D', 
+      background: img.background4,
+    },     
+    title: 'PhysioMap', 
+    description: 'A collaboration done with Physical Therapy Researchers to create a patient-centered app For communicating fall-risk to geriatric patients.',
+    links: [
+      {url: "https://github.com/michael-fernandes", text: "Code"}
+    ]
+  }, 
+  { 
+    style: {
+      color: '#FF2A29',
+      background: img.background2,
+    },
+    title: 'PhysioMap', 
+    description: 'A collaboration done with Physical Therapy Researchers to create a patient-centered app For communicating fall-risk to geriatric patients.',
+    links: [
+      {url: "https://github.com/michael-fernandes", text: "Code"}
+    ]
+  }, 
+];
