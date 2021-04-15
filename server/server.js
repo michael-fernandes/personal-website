d012cf4552f88f55133a175197646f6658c24a97
Michael Fernandes <fernb0t@Kaia-Lynnes-MBP.localdomain>
2021-04-14 23:05:08 -0700

    serve images

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ app.use('^/$', (req, res, next) => {
 })
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')))
+app.use(express.static(path.resolve(__dirname, 'public')))
 
 
 app.listen(PORT, () => {
