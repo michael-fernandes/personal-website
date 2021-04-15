0c573dc7c742886545c5aee3c8c275075444c7d4
Michael Fernandes <fernb0t@Kaia-Lynnes-MBP.localdomain>
2021-04-14 21:23:46 -0700

    eject to fix styled component ssr

--- src/Components/ui/Cards/Card.jsx
+++ src/Components/ui/Cards/Card.jsx
@@ -3,25 +3,25 @@ import { CardLink, CardLinks } from '.';
 import CardTheme from './CardTheme';
 import styled from 'styled-components';
 
-const CardBody = styled.div`
-  background: ${props => props.color};
-  background-image: url(${props => props.background});
-  background-size: cover;
-  background-position: center;
-  padding: 10px;
-`;
-
 function Card({children, theme, links = []}) {
   const {url: primaryUrl, text: primaryText} = links[0];
+
+ const style = { // TODO: figure out why ssr won't allow styled component here
+   backgroundImage: `url(${theme.background})`,
+   background: theme.color, 
+   backgroundSize: 'cover',
+   backgroundPosition: 'center',
+   padding: '10px',
+ }
 
   return (
     <CardTheme.Provider value={theme}>
       {/* <div className="card" style={{border: `1.5px solid ${color}`}}> */}
       <div className="card">
         <a  className="body-link" title={primaryText} href={primaryUrl}>
-          <CardBody className="card-body" color={theme.color} background={theme.background}>
+          <div style={style} className="card-body" color={theme.color} background={theme.background}>
             {children}
-          </CardBody>
+          </div>
         </a>
         <CardLinks>
             {links.map(({url, text}) => <CardLink url={url} text={text}/>)}
