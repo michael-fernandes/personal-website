c240a4a41f93034a9a3603802a4c7c6673873cb1
Michael Fernandes <fernb0t@Kaia-Lynnes-MBP.localdomain>
2021-04-25 20:42:50 -0700

    Update cards

--- src/Containers/ProjectsModal.jsx
+++ src/Containers/ProjectsModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Modal } from 'antd';
+import { Modal, Button } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
+import '../Styles/Modal.css';
 
 import { mockupsVisibility, showModal, hideModal } from '../store';
 
@@ -9,12 +10,24 @@ export default function ProjectsModal() {
   const isModalVisible = useSelector(mockupsVisibility);
   const dispatch = useDispatch();
   return(
+
     <Modal visible={isModalVisible} onOk={() => dispatch(hideModal())} bodyStyle={{
       width: '100%',
-      height: '900px',
-    }}>
-      <h2>Proof of concept mockups</h2>
-      <p>Automated diagnostic system to help patients self service medical devices.</p>
+      // height: '900px',
+    }}
+    footer={[
+      <Button key='cool' onClick={() => dispatch(hideModal())} type="link" color="#6F64F4">
+        Close
+      </Button>
+    ]}
+    >
+      <div style={{maxWidth: "100%"}}>
+        <h1>Proof of concept mockups</h1>
+        <p>Automated diagnostic system to help patients self service medical devices.</p>
+        <img className="modal-photo" src="https://s3-us-west-2.amazonaws.com/mferns.com/phone-normal.png" alt="mockup of phone alert"/>
+        <img className="modal-photo"  src="https://s3-us-west-2.amazonaws.com/mferns.com/phone-alert.png" alt="mockup of phone alert"/>
+      </div>
+      
     </Modal>
   );
 }
--- src/static/cardContent.js
+++ src/static/cardContent.js
@@ -6,22 +6,22 @@ export default [
   { 
     style: {
       color: '#FFA600', 
-      background: isDev ? img.background1 : 'http://mferns.com/backgrounds/background-01.svg',
+      background: isDev ? img.background1 : 'https://s3-us-west-2.amazonaws.com/mferns.com/background-01.svg',
     },
     title: 'PhysioMap', 
-    description: 'A collaboration done with Physical Therapy Researchers to create a patient-centered app For communicating fall-risk to geriatric patients.',
+    description: 'A collaboration done with Physical Therapy Researchers to create a patient-centered app communicating fall-risk to geriatric patients.',
     links: [
-      {url: "http://staff.washington.edu/mfern93/", text: "Website"},
-      {url: "https://github.com/michael-fernandes/physio-map", text: "Code"},
+      {url: "http://staff.washington.edu/mfern93/", text: "PhysioMap"},
+      {url: "https://github.com/michael-fernandes/physio-map", text: "View Code"},
     ]
   },
   { 
     style: {
       color: '#64E6B6', 
-      background: isDev ? img.background3 : 'http://mferns.com/backgrounds/background-03.svg',
+      background: isDev ? img.background3 : 'https://s3-us-west-2.amazonaws.com/mferns.com/background-03.svg',
     },
     title: 'Population Health Work', 
-    description: 'Population Health refers to the analysis of health outcomes on a population level. Here are a few public facing tools and data journalism that I have contributed too.',
+    description: 'Population Health refers to the analysis of health outcomes on a population level. Here are a few public facing tools and data journalism pieces that I have contributed too.',
     links: [
       {url: "https://vizhub.healthdata.org/child-mortality", text: "Child Mortality"},
       {url: "https://covid19.healthdata.org/united-states-of-america", text: "COVID-19 Projections"},
@@ -31,32 +31,32 @@ export default [
   { 
     style: {
       color: '#30AFF2', 
-      background: isDev ? img.background2 : 'http://mferns.com/backgrounds/background-02.svg',
+      background: isDev ? img.background2 : 'https://s3-us-west-2.amazonaws.com/mferns.com/background-02.svg',
     },     
     title: 'Data Viz Explorations', 
-    description: 'Explorations and proof of concepts for novel interactive data visualization using D3.js.',
+    description: 'Explorations and proof of concepts for interactive data visualizations using D3.js.',
     links: [
-      {url: "https://observablehq.com/@michael-fernandes", text: "Observable Demos"}
+      {url: "https://observablehq.com/@michael-fernandes", text: "Observable"}
     ]
   }, 
   { 
     style: {
       color: '#F46464', 
-      background: isDev ? img.background4 : 'http://mferns.com/backgrounds/background-04.svg',
+      background: isDev ? img.background4 : 'https://s3-us-west-2.amazonaws.com/mferns.com/background-04.svg',
     },     
     title: 'Bookasorous', 
     description: 'A mobile app built in react native which allows book readers to track new vocabulary words. Work in Progress.',
     links: [
-      {url: "https://github.com/michael-fernandes", text: "Code"}
+      {url: "https://github.com/michael-fernandes", text: "View Code"}
     ]
   }, 
   { 
     style: {
       color: '#6F64F4', 
-      background: isDev ? img.background5 : 'http://mferns.com/backgrounds/background-05.svg',
+      background: isDev ? img.background5 : 'https://s3-us-west-2.amazonaws.com/mferns.com/background-05.svg',
     },     
     title: 'Self Assist Mockups', 
-    description: 'A custom build visualization dashboard for showing opioid deaths in America.',
+    description: "A mockup suggesting an application that gives users feedback on their device usage, helps them self-diagnosis problems with the device and seek help via their provider or device manufacturer.",
     links: [
       {onClick: mockupsOnClick, text: "View Mockups"}
     ]
