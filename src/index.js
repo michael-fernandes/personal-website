650a1c0c73ffe441f7e5b704953b4fb771eee506
Michael Fernandes <fernb0t@Kaia-Lynnes-MBP.localdomain>
2021-05-13 16:50:20 -0700

    Update styles

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import 'antd/dist/antd.css';
 import './Styles/index.css';
+import 'antd/dist/antd.css';
 // import { analytics } from './Analytics';
 
 // if (process.env.NODE_ENV !== 'development') {
